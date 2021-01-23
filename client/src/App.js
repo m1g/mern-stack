c5246b65fbba117b7c585ccbd1d7cb05b28a84c2
m1g <miguelamalcolm@gmail.com>
2021-01-22 20:43:54 -0500

    reverts to classes for now. links redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -7,8 +12,28 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
 
 import './App.css';
+
+//Check for token to keep user logged in TODO: REPLACE WITH COOKIES
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  
+  const decoded = jwt_decode(token); // Decode token and get user info and exp 
+  store.dispatch(setCurrentUser(decoded)); // Set user and isAuthenticated
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser()); // Logout user
+    window.location.href = "./login"; // Redirect to login
+  }
+}
+
 
 export default function App() {
   return (
@@ -19,6 +44,9 @@ export default function App() {
           <Route exact path="/" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <Switch>
+            <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+          </Switch>
         </div>
       </Router>
     </Provider>
