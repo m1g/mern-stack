467a750d4f461dcf4423d7913b40b5392b95c00c
m1g <miguelamalcolm@gmail.com>
2021-01-22 21:57:46 -0500

    Revert to c5246b65f

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const path = require("path")
 
 const users = require("./routes/api/users")
 
@@ -18,7 +17,7 @@ app.use(
 app.use(bodyParser.json());
 
 // DB Config
-const db = process.env.MONGO_URI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose.connect(db, {
@@ -39,18 +38,6 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
-
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-} else {
-  require('dotenv').config();
-}
 
 const port = process.env.PORT || 5000;
 
